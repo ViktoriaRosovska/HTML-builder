9118f6a3927718deca7e22ac348fb3005a6f254e
Viktoria Rosovska <torimeister@gmail.com>
2025-01-19 03:32:16 +0200

    feat: add script for task 04-copy-directory according to the requirements

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require("path");
+
+async function copyDir(inputDir, outputDir) {
+    try {
+        
+        await fs.promises.mkdir(outputDir, { recursive: true, force: true });
+       
+        const copyItems = await fs.promises.readdir(inputDir, { withFileTypes: true });
+        items = [];
+        copyItems.forEach(async file =>  {
+            items.push(file.name);
+            if (file.isDirectory()) {
+                copyDir(path.join(inputDir, file.name), path.join(outputDir, file.name));
+            }
+             else if (file.isFile()) {
+                await fs.promises.copyFile(path.join(inputDir, file.name), path.join(outputDir, file.name));
+            }
+            
+        });
+        const copyOutputItems = await fs.promises.readdir(outputDir, { withFileTypes: true });
+        copyOutputItems.forEach(async file => {
+            if (!items.includes(file.name)) {
+                await fs.promises.rm(path.join(outputDir, file.name), { recursive: true, force: true }, err => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`${path.join(outputDir, file.name)} is deleted!`);
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+copyDir(path.join(__dirname, "files"), path.join(__dirname, "files-copy"));
