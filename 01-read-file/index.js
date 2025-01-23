e9159e9fd2e5b02211c25942e6d4ae4e20d0f5d6
Viktoria Rosovska <torimeister@gmail.com>
2025-01-23 10:54:54 +0200

    fix: 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,4 +4,4 @@ const path = require('path');
 const stream = fs.createReadStream(path.join(__dirname, "text.txt"));
 let data = "";
 stream.on("data", (chunk) => (data += chunk));
-stream.on("end", () => console.log("End", data));
+stream.on("end", () => console.log(data));
